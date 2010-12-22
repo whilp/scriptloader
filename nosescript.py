9b600eb27c505f71e2ff126934d7c35327bbc63c
Will Maier <willmaier@ml1.net>
2010-12-22 15:19:56 -0600

    Make the plugin work.

--- nosescript.py
+++ nosescript.py
@@ -5,10 +5,19 @@ import nose.plugins
 
 class NoseScript(nose.plugins.Plugin):
 
+    def options(self, parser, env):
+        return nose.plugins.Plugin.options(self, parser, env)
+
+    def configure(self, options, conf):
+        return nose.plugins.Plugin.configure(self, options, conf)
+
+    def prepareTestLoader(self, loader):
+        self.loader = loader
+
     def loadTestsFromFile(self, filename):
         try:
             module = imp.load_source("module", filename)
         except SyntaxError:
             return False
 
-        return self.loadTestsFromModule(module)
+        return self.loader.loadTestsFromModule(module)
