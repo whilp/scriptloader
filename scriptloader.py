85d0e4a7c49ba1f0929008fba153e9c2bf3ce3f1
Will Maier <willmaier@ml1.net>
2010-12-22 16:03:46 -0600

    Docstrings.

--- scriptloader.py
+++ scriptloader.py
@@ -12,9 +12,21 @@ class ScriptLoader(nose.plugins.Plugin):
         return nose.plugins.Plugin.configure(self, options, conf)
 
     def prepareTestLoader(self, loader):
+        """Save the *loader*.
+
+        This instance will be used later by :meth:`loadTestsFromFile` if
+        necessary.
+        """
         self.loader = loader
 
     def loadTestsFromFile(self, filename):
+        """Load tests from *filename*.
+
+        Attempt to load the file using :func:`imp.load_source`. If that
+        succeeds, pass the loaded module to the :attr:`loader`
+        :meth:`loadTestsFromModule`. If the file can't be loaded, return False
+        so other plugins can try loading it.
+        """
         try:
             module = imp.load_source("module", filename)
         except SyntaxError:
