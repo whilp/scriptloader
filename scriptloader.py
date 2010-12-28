60f124de09a2aa7149356ad8ff8e5e070dd24ef8
Will Maier <willmaier@ml1.net>
2010-12-28 16:59:02 -0600

    Add support for test selection within non-module files.

--- scriptloader.py
+++ scriptloader.py
@@ -1,7 +1,11 @@
 import imp
 
+import nose.failure
 import nose.loader
 import nose.plugins
+import nose.selector
+import nose.suite
+import nose.util
 
 class ScriptLoader(nose.plugins.Plugin):
     """Load tests from scripts that may not have a .py extension."""
@@ -34,3 +38,34 @@ class ScriptLoader(nose.plugins.Plugin):
             return None
 
         return self.loader.loadTestsFromModule(module)
+    
+    def loadTestsFromName(self, name, module=None, discovered=False):
+        """Load tests from the entity with the given *name*.
+
+        Attempt to loa
+        """
+        addr = nose.selector.TestAddress(name, workingDir=self.loader.workingDir)
+        path = addr.filename
+        if not path:
+            return self.loader.loadTestsFromName(name, module=module, discovered=discovered)
+
+        module = getpackage(path)
+        if module is None:
+            try:
+                module = imp.load_source("module", path)
+            except SyntaxError:
+                return
+
+        suite = self.loader.suiteClass
+        if addr.call:
+            name = addr.call
+        parent, obj = self.loader.resolve(name, module)
+        if (nose.util.isclass(parent)
+                and getattr(parent, '__module__', None) != module.__name__):
+            parent = nose.util.transplant_class(parent, module.__name__)
+            obj = getattr(parent, obj.__name__)
+        if isinstance(obj, nose.failure.Failure):
+            return suite([obj])
+        else:
+            return suite(nose.suite.ContextList([self.loader.makeTest(obj, parent)],
+                context=parent))
